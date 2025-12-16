68badfa7c256007506c7b5f0d184497f67258a14
SwaroopDangal <swaroopdangal732@gmail.com>
2025-12-16 17:29:08 +0545

    middleware,onboarding endpoint added

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { login, signup } from "../controllers/auth.controllers.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
+router.post("/onboarding", protectRoute, onboard);
 
 export default router;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
+import cookieParser from "cookie-parser";
 import { connectDB } from "../src/lib/db.js";
 dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT;
 
-app.get("/", (req, res) => {
-  res.send("Hello world");
-});
-
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
