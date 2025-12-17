9b0c0c14c17ab52847de76045fcca058ee2311e6
SwaroopDangal <swaroopdangal732@gmail.com>
2025-12-17 20:24:07 +0545

    setup tanstackquery,axios,react-router and created signup page

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -1,28 +1,28 @@
-import jwt from "jsonwebtoken";
-import User from "../models/User.models.js";
+  import jwt from "jsonwebtoken";
+  import User from "../models/User.models.js";
 
-export const protectRoute = async (req, res, next) => {
-  try {
-    const token = req.cookies.jwt;
-    if (!token) {
-      return res
-        .status(401)
-        .json({ message: "Unauthorized -No token provided" });
-    }
-    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
-    if (!decoded) {
-      return res.status(401).json({ message: "Unauthorized - Invalid token" });
-    }
+  export const protectRoute = async (req, res, next) => {
+    try {
+      const token = req.cookies.jwt;
+      if (!token) {
+        return res
+          .status(401)
+          .json({ message: "Unauthorized -No token provided" });
+      }
+      const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+      if (!decoded) {
+        return res.status(401).json({ message: "Unauthorized - Invalid token" });
+      }
 
-    const user = await User.findById(decoded.userId);
-    if (!user) {
-      return res.status(401).json({ message: "Unauthorized - User not found" });
-    }
+      const user = await User.findById(decoded.userId);
+      if (!user) {
+        return res.status(401).json({ message: "Unauthorized - User not found" });
+      }
 
-    req.user = user;
-    next();
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({ message: "Internal server error" });
-  }
-};
+      req.user = user;
+      next();
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ message: "Internal server error" });
+    }
+  };
--- backend/src/server.js
+++ backend/src/server.js
@@ -4,12 +4,18 @@ import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import chatRoutes from "./routes/chat.routes.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import { connectDB } from "../src/lib/db.js";
 dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT;
-
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
