969eaabe71ccac15cae4ff563cf14e55b0587051
SwaroopDangal <swaroopdangal732@gmail.com>
2025-12-20 10:19:31 +0545

    created call page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -70,8 +70,16 @@ const App = () => {
           }
         />
         <Route
-          path="/call"
-          element={isAuthenticated ? <CallPage /> : <Navigate to={"/login"} />}
+          path="/call/:id"
+          element={
+            isAuthenticated && isOnboarded ? (
+              <Layout showSidebar={false}>
+                <CallPage />
+              </Layout>
+            ) : (
+              <Navigate to={!isAuthenticated ? "/login" : "/onboarding"} />
+            )
+          }
         />
         <Route
           path="/chat/:id"
